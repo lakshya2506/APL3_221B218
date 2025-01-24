c8a54c7bc079feb48109961cd10d9a741ba9d4bb
221B218 <221B218@juet.co.in>
2025-01-24 14:21:59 +0530

    Lab Exercise 1

---
+++ LE1/Prob3/Main.java
@@ -0,0 +1,13 @@
+
+
+public class Main
+{
+	public static void main(String[] args) {
+		Mother m = new Mother();
+		m.show();
+		Mother m1 = new Mother();
+		m1.show();
+		Child ch = new Child();
+		ch.show();
+	}
+}
