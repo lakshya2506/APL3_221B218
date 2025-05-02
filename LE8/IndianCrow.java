0c7563a6b7ce2f382cdd96ae1b21e4fd88e990d6
221B218 <221B218@juet.co.in>
2025-05-02 15:43:28 +0530

    Lab Exercise 8

---
+++ LE8/IndianCrow.java
@@ -0,0 +1,13 @@
+public class IndianCrow implements Crow {
+    public void eat() {
+        System.out.println("The Crow is eating.");
+    }
+
+    public void cry() {
+        System.out.println("The Crow is singing.");
+    }
+
+    public void fly() {
+        System.out.println("The Crow is swimming.");
+    }
+}
---
+++ LE8/Swan.java
@@ -0,0 +1,5 @@
+public interface Swan {
+    public abstract void eat();
+    public abstract void sing();
+    public abstract void swim();
+}
